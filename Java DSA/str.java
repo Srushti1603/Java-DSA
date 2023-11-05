7f87a9aba485e2af12e771771df5212ab2322efe
Srushti Jalgaonkar <147828302+Srushti1603@users.noreply.github.com>
2023-11-05 17:30:01 +0530

    Add files via upload

---
+++ Java DSA/str.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class str {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Your Name");
+        String str = scn.nextLine();
+        System.out.println("Entered Name Is "+str);
+        scn.close();
+    }
+}
