f7c40ac339dcd15e80f0f415fc5460034d2319ba
Srushti Jalgaonkar <147828302+Srushti1603@users.noreply.github.com>
2023-12-27 18:37:57 +0530

    Add files via upload

---
+++ Java DSA/toggle.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class toggle2 {
+
+    static void TOGGLE(StringBuilder S)
+    {
+        for(int i=0;i<S.length();i++)
+        {
+            char ch = S.charAt(i);
+            if(ch == ' ') continue;
+            int ascii = (int)ch;
+            boolean flag=true;
+
+            if(ascii>=97) flag=false;
+
+            if(flag==true)
+            {
+                ascii = ascii + 32;
+                char dh = (char)ascii;
+                S.setCharAt(i,dh);
+            }
+            else
+            {
+                ascii = ascii-32;
+                char dh = (char)ascii;
+                S.setCharAt(i,dh);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter string : ");
+        StringBuilder sb = new StringBuilder(scn.nextLine());
+
+        System.out.println("Toggle the string -->");
+        TOGGLE(sb);
+        System.out.println(sb);
+        scn.close();
+    }
+}
